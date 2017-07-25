c217c19fad975d84184befc60820d10e176272ea
JohnOmernik <john@omernik.com>
2017-07-25 14:53:24 +0000

    Updated Sound

--- base_install/install.sh
+++ base_install/install.sh
@@ -16,3 +16,14 @@ git clone https://github.com/adafruit/Adafruit_Python_GPIO.git && cd Adafruit_Py
 # Adafruit PWM Servo Library
 git clone https://github.com/adafruit/Adafruit_Python_PCA9685.git && cd Adafruit_Python_PCA9685 && sudo python setup.py install && cd ..
 
+
+
+echo "Updating sound driver to prefer USB audio"
+sudo tee /etc/asound.conf << EOF
+pcm.!default  {
+ type hw card 1
+}
+ctl.!default {
+ type hw card 1
+}
+EOF
