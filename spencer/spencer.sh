292993304aab438adff52c1c47d212e14d60c227
johnomernik <john@omernik.com>
2017-09-22 08:36:18 -0500

    Added mixer setting in specer

--- spencer/spencer.sh
+++ spencer/spencer.sh
@@ -8,6 +8,8 @@
 # Short-Description: Spencer Zapping
 ### END INIT INFO
 DAEMON="spencer"
+# Force 3.5mm audio if Pi 3
+#sudo amixer cset numid=3 1
 script="/home/pi/pimeup/${DAEMON}/${DAEMON}.py"
 /usr/bin/python $script &
 
