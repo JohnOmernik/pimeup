ecb5cae3fa5b967b2436253d6887726d0b7bf5bc
johnomernik <john@omernik.com>
2017-10-14 14:57:57 -0500

    Glove working!

--- hand_servo/JO_flex.py
+++ hand_servo/JO_flex.py
@@ -4,65 +4,83 @@ import time
 import random
 import sys
 import socket
-import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_MCP3008
 
 
-# change these as desired - they're the pins connected from the
-# SPI port on the ADC to the Cobbler
-SPICLK = 18
-SPIMISO = 23
-SPIMOSI = 24
-SPICS = 25
- 
-# set up the SPI interface pins
-GPIO.setup(SPIMOSI, GPIO.OUT)
-GPIO.setup(SPIMISO, GPIO.IN)
-GPIO.setup(SPICLK, GPIO.OUT)
-GPIO.setup(SPICS, GPIO.OUT)
+# Hardware SPI configuration:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
-DEBUG = 1
+tdelay = 0.2
+DEBUG = 0
+NETWORK = 1
+SHOWLIST = [ "Pinky", "Index", "Thumb" ]
 
-SENSORS = {}
-SENSORS["MIDDLE"] = {"ADC": 0, "REMOTE": 4, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5}
+SENSORS = [
+    {"NAME": "Pinky", "REMOTE": 4, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
+    {"NAME": "Ring", "REMOTE": 3, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
+    {"NAME": "Middle", "REMOTE": 2, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
+    {"NAME": "Index", "REMOTE": 1, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
+    {"NAME": "Thumb", "REMOTE": 0, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5}
+]
+
+
+#SERVOS.append({"DESC":"Thumb", "RANGE_MIN": 275, "RANGE_MAX": 575, "INVERT": False})
+#SERVOS.append({"DESC":"Pointer", "RANGE_MIN": 300, "RANGE_MAX": 575, "INVERT": True})
+#SERVOS.append({"DESC":"Middle", "RANGE_MIN": 325, "RANGE_MAX": 575, "INVERT": True})
+#SERVOS.append({"DESC":"Ring", "RANGE_MIN":  275, "RANGE_MAX": 550, "INVERT": True})
+#SERVOS.append({"DESC":"Pinky", "RANGE_MIN": 300, "RANGE_MAX": 575, "INVERT": True})
+#SERVOS.append({"DESC":"WristFlex", "RANGE_MIN": 300, "RANGE_MAX": 600, "INVERT": False})
+#SERVOS.append({"DESC":"WristTurn", "RANGE_MIN": 135, "RANGE_MAX": 660, "INVERT": False})
+#SERVOS.append({"DESC":"WristUp", "RANGE_MIN": 360, "RANGE_MAX" : 620, "INVERT": False})
+
+
 
 
 def main():
     #flex sensor
-    TCP_IP = '192.168.0.122'
-    TCP_PORT = 30000
-    tdelay = 0.2
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((TCP_IP, TCP_PORT))
-    except:
-        print("Error connecting to %s:%s" % (TCP_IP, TCP_PORT))
-        sys.exit(1)
+
+
+    if NETWORK == 1:
+        TCP_IP = '192.168.0.130'
+        TCP_PORT = 30000
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((TCP_IP, TCP_PORT))
+        except:
+            print("Error connecting to %s:%s" % (TCP_IP, TCP_PORT))
+            sys.exit(1)
+    else:
+        s = ""
 
 
     try:
         while True:
         # read the analog pin
-            flex_middle = readadc(SENSORS['MIDDLE']['ADC'], SPICLK, SPIMOSI, SPIMISO, SPICS)
-            procValue('MIDDLE', flex_middle, s)            
+            for x in range(len(SENSORS)):
+                flex_val = mcp.read_adc(x)
+                procValue(x, flex_val, s)
             time.sleep(tdelay)
     except KeyboardInterrupt:
         print("")
         print("Weee - Exiting")
         s.close()
-        GPIO.cleanup()  
         sys.exit(0)
 
 
 def procValue(s, v, c):
     global SENSORS
 
+    curname = SENSORS[s]['NAME']
+
     if v > SENSORS[s]['MAX']:
         SENSORS[s]['MAX'] = v
     if v < SENSORS[s]['MIN']:
         SENSORS[s]['MIN'] = v
-    print(SENSORS[s])
-    
+    if DEBUG:
+        print(SENSORS[s])
     if SENSORS[s]['MAX'] == SENSORS[s]['MIN']:
         SENSORS[s]['MAX'] += 1
     if DEBUG:
@@ -73,59 +91,29 @@ def procValue(s, v, c):
     sendval = (float(v) - SENSORS[s]['MIN']) / (SENSORS[s]['MAX'] - SENSORS[s]['MIN'])
     sendval = int(sendval * 100)
 
-    sendstr = str(SENSORS[s]['REMOTE']) + ":" + str(sendval)
+    if sendval < 10:
+        pad = "00"
+    elif sendval < 100:
+        pad = "0"
+    else:
+        pad = ""
 
-    print("Sendval: %s" % sendval)
+    sendstr = str(SENSORS[s]['REMOTE']) + ":" + pad + str(sendval)
+    if DEBUG:
+        print("Sendval: %s" % sendval)
 
     sense_delta = abs(v - SENSORS[s]['LAST'])
+
     if sense_delta > SENSORS[s]['THRES']:
         SENSORS[s]['CHANGES'] += 1
         if SENSORS[s]['CHANGES'] > 5:
             if sendstr != "":
-                print ("**********************Sending this: %s " % sendstr)
-                c.send(sendstr)
+                if curname in SHOWLIST:
+                    print ("******** %s - Sending this: %s " % (curname, sendstr))
+                if NETWORK:
+                    c.send(sendstr)
                 sendstr = ""
     SENSORS[s]['LAST'] = v
-            # Now send the changes to the server!
-            
-
-
-
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-    if ((adcnum > 7) or (adcnum < 0)):
-        return -1
-    GPIO.output(cspin, True)
-    GPIO.output(clockpin, False)  # start clock low
-    GPIO.output(cspin, False)     # bring CS low
- 
-    commandout = adcnum
-    commandout |= 0x18  # start bit + single-ended bit
-    commandout <<= 3    # we only need to send 5 bits here
-    for i in range(5):
-        if (commandout & 0x80):
-            GPIO.output(mosipin, True)
-        else:
-            GPIO.output(mosipin, False)
-        commandout <<= 1
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
- 
-    adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-    for i in range(12):
-        GPIO.output(clockpin, True)
-        GPIO.output(clockpin, False)
-        adcout <<= 1
-        if (GPIO.input(misopin)):
-            adcout |= 0x1
- 
-    GPIO.output(cspin, True)
-        
-    adcout >>= 1       # first bit is 'null' so drop it
-    return adcout
-
-
 
 if __name__ == "__main__":
     main()
