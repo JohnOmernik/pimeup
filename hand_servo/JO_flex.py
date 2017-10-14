5158b47b88696e6d7d8fcdeb514b2f14205144b5
johnomernik <john@omernik.com>
2017-10-14 15:28:56 -0500

    Updated flex

--- hand_servo/JO_flex.py
+++ hand_servo/JO_flex.py
@@ -16,14 +16,14 @@ mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 tdelay = 0.2
 DEBUG = 0
 NETWORK = 1
-SHOWLIST = [ "Pinky", "Index", "Thumb" ]
+SHOWLIST = [ "Pinky", "Ring", "Middle", "Index", "Thumb" ]
 
 SENSORS = [
-    {"NAME": "Pinky", "REMOTE": 4, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
-    {"NAME": "Ring", "REMOTE": 3, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
-    {"NAME": "Middle", "REMOTE": 2, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
-    {"NAME": "Index", "REMOTE": 1, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5},
-    {"NAME": "Thumb", "REMOTE": 0, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 5}
+    {"NAME": "Pinky", "REMOTE": 4, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 2},
+    {"NAME": "Ring", "REMOTE": 3, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 2},
+    {"NAME": "Middle", "REMOTE": 2, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 2},
+    {"NAME": "Index", "REMOTE": 1, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 2},
+    {"NAME": "Thumb", "REMOTE": 0, "MIN":1000, "MAX": 0, "CHANGES":0, "LAST": 0, "CHANGED": False, "THRES": 2}
 ]
 
 
@@ -38,20 +38,24 @@ SENSORS = [
 
 
 
+IP = '192.168.0.130'
+PORT = 30000
 
 def main():
     #flex sensor
 
 
     if NETWORK == 1:
-        TCP_IP = '192.168.0.130'
-        TCP_PORT = 30000
-        try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            s.connect((TCP_IP, TCP_PORT))
-        except:
-            print("Error connecting to %s:%s" % (TCP_IP, TCP_PORT))
-            sys.exit(1)
+#        try:
+#            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#            s.connect((IP, PORT))
+#        except:
+#            print("Error connecting to %s:%s" % (IP, PORT))
+#            sys.exit(1)
+
+        s = socket.socket(socket.AF_INET, # Internet
+                          socket.SOCK_DGRAM) # UDP
+
     else:
         s = ""
 
@@ -79,14 +83,8 @@ def procValue(s, v, c):
         SENSORS[s]['MAX'] = v
     if v < SENSORS[s]['MIN']:
         SENSORS[s]['MIN'] = v
-    if DEBUG:
-        print(SENSORS[s])
     if SENSORS[s]['MAX'] == SENSORS[s]['MIN']:
         SENSORS[s]['MAX'] += 1
-    if DEBUG:
-        print ("v: %s" %v)
-        print ("Max: %s" % SENSORS[s]['MAX'])
-        print ("Min: %s" % SENSORS[s]['MIN'])
 
     sendval = (float(v) - SENSORS[s]['MIN']) / (SENSORS[s]['MAX'] - SENSORS[s]['MIN'])
     sendval = int(sendval * 100)
@@ -100,7 +98,8 @@ def procValue(s, v, c):
 
     sendstr = str(SENSORS[s]['REMOTE']) + ":" + pad + str(sendval)
     if DEBUG:
-        print("Sendval: %s" % sendval)
+        if curname in SHOWLIST:
+            print("%s - Value: %s - Min: %s - Max: %s - Sendval: %s" % (curname, v, SENSORS[s]['MIN'], SENSORS[s]['MAX'], sendval))
 
     sense_delta = abs(v - SENSORS[s]['LAST'])
 
@@ -111,7 +110,7 @@ def procValue(s, v, c):
                 if curname in SHOWLIST:
                     print ("******** %s - Sending this: %s " % (curname, sendstr))
                 if NETWORK:
-                    c.send(sendstr)
+                    c.sendto(sendstr, (IP, PORT))
                 sendstr = ""
     SENSORS[s]['LAST'] = v
 
