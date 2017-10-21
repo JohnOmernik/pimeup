f064df3dc60ad09ef0de6facf8e58a6c1c3c045b
JohnOmernik <john@omernik.com>
2017-10-20 19:24:59 -0500

    Updated audio for torture roome in torture2

---
+++ logviewer/logviewer.py
@@ -0,0 +1,127 @@
+#!/usr/bin/python3
+from flask import Flask, abort, jsonify, request
+import subprocess
+import io
+import json
+import socket
+import os
+import sys
+import time
+from collections import OrderedDict
+
+WHOAMI = socket.gethostname()
+WHATAMI = os.path.basename(__file__).replace(".py", "")
+
+logrefresh = 5
+lastrefresh = 0
+numlogs = 10
+app = Flask(__name__)
+
+curtime = int(time.time())
+curday = time.strftime('%Y-%m-%d', time.localtime(curtime))
+
+logdir = "/home/pi/pimeup/hauntlogs/logs/" + curday
+people = {}
+displaylogs = {}
+stats = {}
+
+def reloaddisplay():
+    global displaylogs
+    for filename in os.listdir(logdir):
+
+
+        p = subprocess.Popen(['tail', '-' + str(numlogs), logdir + "/" + filename], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        retcode = None
+        output = "["
+        while retcode is None:
+            retcode = p.poll() #returns None while subprocess is running
+            try:
+                for line in io.TextIOWrapper(p.stdout, encoding="utf-8"):
+                    output = output + line + ","
+            except:
+                break
+        output = output[0:-1]
+        output += "]"
+        try:
+            jlast = json.loads(output)
+        except:
+            print(output)
+        displaylogs[filename.replace(".log", "")] = jlast
+
+
+def gatherstats():
+    global displaylogs
+    global stats
+    global people
+    curtime = int(time.time())
+    people = {}
+    for x in displaylogs.keys():
+        tstat = {}
+        tstat["piname"] = x
+        tstat["bsname"] = ""
+        tstat["roomname"] = ""
+        tstat["lastts"] = ""
+        tstat["lastroomhb"] = ""
+        tstat["lastbshb"] = ""
+        tstat["wiilevel"] = 0
+
+        for y in displaylogs[x]:
+            logtime = int(time.mktime(time.strptime(y['ts'], '%Y-%m-%d %H:%M:%S')))
+            if y['script'].find("bspimeup") >= 0:
+                tstat["bsname"] = y['script']
+                tstat["lastbshb"] = y['ts']
+            else:
+                tstat["roomname"] = y['script']
+                tstat["lastroomhb"] = y['ts']
+            tstat["lastts"] = y['ts']
+            if y['event_desc'] == 'wii HB':
+                tstat["wiilevel"] = y['event_data']
+            if y['script'].find("bspimeup") < 0 and y['event_type'].find('heartbeat') < 0:
+                if curtime - logtime < 30:
+                    if x not in people:
+                        people[x] = []
+                    people[x].append(y)
+        tstat["lastepoch"] = int(time.mktime(time.strptime(tstat['lastts'], '%Y-%m-%d %H:%M:%S')))
+        tstat["tdelta"] = curtime - tstat["lastepoch"]
+        if tstat["tdelta"] >= 60:
+            tstat["WARNING"] = "THISROOMDELTA IS LARGER then 60"
+        stats[x] = tstat
+
+
+    
+@app.route('/showlogs', methods=['GET'])
+def showlogs():
+    global logrefresh
+    global lastrefresh
+
+    curtime = int(time.time())
+    if curtime - lastrefresh >= logrefresh:
+        reloaddisplay()
+        gatherstats()
+
+
+
+    return jsonify(displaylogs), 201
+@app.route('/showstats', methods=['GET'])
+def showstats():
+    global logrefresh
+    global lastrefresh
+
+    curtime = int(time.time())
+    if curtime - lastrefresh >= logrefresh:
+        reloaddisplay()
+        gatherstats()
+
+
+    out = {}
+    for x in stats:
+        if x not in out:
+            out[x] = stats[x]
+    for x in people:
+        out[x]["people"] = people[x]
+    
+    return jsonify(out), 201
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5060, debug=True)
---
+++ torture2/torture2.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import time
+import random
+import sys
+import alsaaudio
+import json
+import requests
+import os
+import socket
+from collections import OrderedDict
+
+WHOAMI = socket.gethostname()
+WHATAMI = os.path.basename(__file__).replace(".py", "")
+
+#m = alsaaudio.Mixer('PCM')
+#current_volume = m.getvolume() # Get the current Volume
+#print("Cur Vol: %s " % current_volume)
+#m.setvolume(100) # Set the volume to 70%.
+#current_volume = m.getvolume() # Get the current Volume
+#print("Cur Vol: %s " % current_volume)
+
+hbinterval = 30
+lasthb = 0
+
+
+def main():
+    logevent("startup", "startup", "Just started and ready to run")
+
+    global lasthb
+    global hbinterval
+
+    sounds = [0, 0, 0]
+    channels = 2
+    rate = 44100
+    size = 1024
+
+    out_stream = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK, alsaaudio.PCM_NORMAL, 'default')
+#    out_stream.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+#    out_stream.setchannels(channels)
+#    out_stream.setrate(rate)
+#    out_stream.setperiodsize(size)
+
+
+    soundfiles = ['/home/pi/torture_audio.wav']
+
+    while True:
+        curfile = random.choice(soundfiles)
+        curtime = int(time.time())
+        if curtime - lasthb > hbinterval:
+            logevent("heartbeat", "Working", "Cur Childhood sound file: %s" % curfile)
+            lasthb = curtime
+        curstream = open(curfile, "rb")
+        data = curstream.read(size)
+        while data:
+            out_stream.write(data)
+            data = curstream.read(size)
+        curstream.close()
+
+    sys.exit(0)
+
+def logevent(etype, edata, edesc):
+    global WHOAMI
+    global WHATAMI
+
+    curtime = int(time.time())
+    curts = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(curtime))
+    outrec = OrderedDict()
+    outrec['ts'] = curts
+    outrec['host'] = WHOAMI
+    outrec['script'] = WHATAMI
+    outrec['event_type'] = etype
+    outrec['event_data'] = edata
+    outrec['event_desc'] = edesc
+    sendlog(outrec, False)
+    outrec = None
+
+def sendlog(log, debug):
+    logurl = "http://hauntcontrol:5050/hauntlogs"
+    try:
+        r = requests.post(logurl, json=log)
+        if debug:
+            print("Posted to %s status code %s" % (logurl, r.status_code))
+            print(json.dumps(log))
+    except:
+        if debug:
+            print("Post to %s failed timed out?" % logurl)
+            print(json.dumps(log))
+
+
+if __name__ == "__main__":
+    main()
